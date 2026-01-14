4555cd8468a36091648c24c841fe8f9a2e94d2da
Danel <danel.rivas@ikasle.egibide.org>
2026-01-14 09:11:55 +0100

    Visualizar datos Admin intento2
    Vaya liada

---
+++ frontend/src/pages/Admin/empresas.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { onMounted } from 'vue';
+  import { useEmpresasStore } from '@/stores/empresas';
+
+  const empresasStore = useEmpresasStore();
+
+  onMounted(() => {
+    empresasStore.fetchEmpresas();
+  });
+</script>
+
+<template>
+  <div>
+    <h2>Lista de Empresas</h2>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="empresa in empresasStore.empresas" :key="empresa.id">
+        {{ empresa.nombre }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
