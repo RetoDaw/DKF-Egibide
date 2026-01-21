f395e20d4b49b45c49d245869fc49770830161da
EnekoYranzo-Ikasle <eneko.yranzo.48852@ikasle.egibide.org>
2026-01-21 20:54:32 +0100

    Arreglar rutas en la rama

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -150,7 +150,7 @@ const router = createRouter({
         },
         {
           path: "tutor-egibide/alumno-empresa/:alumnoId/calificaciones",
-          name: "tutor_egibide-calificaciones",
+          name: "tutor_egibide-cegalificaciones",
           components: {
             main: () => import("@/pages/TutorEgibide/calificaciones.vue"),
           },
